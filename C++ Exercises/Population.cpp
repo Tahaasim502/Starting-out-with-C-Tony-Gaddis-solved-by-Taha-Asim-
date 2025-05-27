21f5c7a6e64c02f3b585f00b31f1074ca4f29b8f
Tahaasim502 <tahaasim40@gmail.com>
2025-05-27 13:48:52 +0500

    Add files via upload

--- C++ Exercises/Population.cpp
+++ C++ Exercises/Population.cpp
@@ -1,48 +1,39 @@
 #include<iostream>
 using namespace std;
-void sizePopulation()
-{
-	double startingsize, yearsdisplay, n = 0;
-	double  brate, drate;
-	cout << "Enter the starting size of the population: ";
-	cin >> startingsize;
-	while (startingsize < 2)
-	{
-		cout << "Do not enter a number less than 2!, please re-enter: ";
-		cin >> startingsize;
-	}
-	cout << "Enter the birth rate of the population: ";
-	cin >> brate;
-	while (brate < 0)
-	{
-		cout << "Error, make sure no negative number is entered: ";
-		cin >> brate;
-	}
-	cout << "Enter the death rate of the population: ";
-	cin >> drate;
-	while (drate < 0)
-	{
-		cout << "Error, make sure no negative number is entered: ";
-		cin >> drate;
-	}
-	cout << "Enter the number of years you want to display the population for: ";
-	cin >> yearsdisplay;
-	while (yearsdisplay < 1)
-	{
-		cout << "Error, make sure a number less than 1 is not entered re-enter: ";
-		cin >> yearsdisplay;
-
-	}
-	n = startingsize;
-	for (int i = 1; i <= yearsdisplay; i++)
-	{
-		n = n + ((brate / 100.0) * startingsize) - ((drate / 100.0) * startingsize);
-		cout << "The population size for " << i << " year " << n;
-		cout << endl;
-	}
-}
 int main()
 {
-	sizePopulation();
+	int pop, days;
+	double percentageincrease, newpop, perpop; 
+	cout << "Enter the population of organisms: ";
+	cin >> pop;
+	while (pop < 2)
+	{
+		cout << "Enter the starting size greater than 2: ";
+		cin >> pop;
+	}
+	cout << endl;
+	cout << "Enter the number of days you want to notice the increase: ";
+	cin >> days;
+	while (days < 1)
+	{
+		cout << "Enter the number of days greater than 1: ";
+		cin >> days;
+	}
+	cout << endl;
+	cout << "Enter the population increase: ";
+	cin >> percentageincrease;
+	while (percentageincrease < 0)
+	{
+		cout << "Do not enter a negative number: ";
+		cin >> percentageincrease;
+	}
+	cout << endl;
+	perpop = percentageincrease / 100.0;
+	newpop = pop; 
+	for (int i = 1; i <= days; i++)
+	{ 
+		cout << "Day " << i << " population increase was " << newpop << endl;
+		newpop += (pop * perpop);
+	}
 	return 0;
 }
