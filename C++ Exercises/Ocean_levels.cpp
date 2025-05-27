10b45279bc6bedcb408959f971b8982f04799053
Tahaasim502 <tahaasim40@gmail.com>
2025-05-27 13:46:14 +0500

    Add files via upload

---
+++ C++ Exercises/Ocean_levels.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double level = 1.5;
+	cout << "year " << " level " << endl;
+	for (int i = 1; i <= 25; i++)
+	{
+		cout << i << "\t" << i * 1.5 << endl;
+	}
+	return 0;
+}
